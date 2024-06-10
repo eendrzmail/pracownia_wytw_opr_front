734f4689f7d75152c03ca6cc63147134401558a3
Andrzej Gebus <andrzejgebuss@gmail.com>
2024-06-10 23:15:39 +0200

    a

--- src/http/AxiosAbstract.ts
+++ src/http/AxiosAbstract.ts
@@ -5,30 +5,30 @@ import mock from './mock';
 export abstract class HttpClient {
 
     public static getCurrentWeather = (city: string) => {
-        // return axiosInstance
-            // .get<IWeather>(`GetCurrentWeather/${city}`)
-            // .then(response => response.data);
+        return axiosInstance
+            .get<IWeather>(`GetCurrentWeather/${city}`)
+            .then(response => response.data);
         return Promise.resolve(mock)
     };
 
     public static getFutureWeather = (city: string, date:string) => {
-        // return axiosInstance
-        //     .get<IWeather>(`GetCurrentWeather/${city}/${date}`)
-        //     .then(response => response.data);
+        return axiosInstance
+            .get<IWeather>(`GetCurrentWeather/${city}/${date}`)
+            .then(response => response.data);
         return Promise.resolve(mock)
     };
 
     public static getForecast = (city: string, days: string | number) => {
-        // return axiosInstance
-        //     .get<IWeather>(`GetCurrentWeather/${city}/${days}`)
-        //     .then(response => response.data);
+        return axiosInstance
+            .get<IWeather>(`GetCurrentWeather/${city}/${days}`)
+            .then(response => response.data);
         return Promise.resolve(mock)
     };
 
     public static getWeatherHistory = (city: string, day: string | number) => {
-        // return axiosInstance
-        //     .get<IWeather>(`GetWeatherHistory/${city}/${day}`)
-        //     .then(response => response.data);
+        return axiosInstance
+            .get<IWeather>(`GetWeatherHistory/${city}/${day}`)
+            .then(response => response.data);
         return Promise.resolve(mock)
     };
 }
